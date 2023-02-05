17b83667f39c456aac4b88666afdc5107874071b
Moritz Hesche <mo.hesche@gmail.com>
2023-02-05 12:04:11 +0100

    more scoring cards

--- carthographRL/game/game.py
+++ carthographRL/game/game.py
@@ -85,7 +85,7 @@ def metropole(field):
 
         area = connected_areas == area_label
         surrounding_mask = ski.segmentation.find_boundaries(
-            area, mode="thick", connectivity=1
+            area, mode="outer", connectivity=1
         )
         if SpaceType.MOUNTAIN.value not in field[np.argwhere(surrounding_mask)]:
             return area_size
@@ -117,7 +117,7 @@ def schillernde_ebene(field):
             continue
 
         surrounding_mask = ski.segmentation.find_boundaries(
-            connected_areas == area_label, mode="thick", connectivity=1
+            connected_areas == area_label, mode="outer", connectivity=1
         )
         surrounding_types = list(np.unique(field[np.argwhere(surrounding_mask)]))
         surrounding_types.remove(SpaceType.EMPTY.value)
@@ -160,7 +160,7 @@ def die_aeusserste_enklave(field):
             continue
 
         surrounding_mask = ski.segmentation.find_boundaries(
-            connected_areas == area_label, mode="thick", connectivity=1
+            connected_areas == area_label, mode="outer", connectivity=1
         )
         surrounding_spaces = field[np.argwhere(surrounding_mask)]
         scores.append(len(surrounding_spaces == SpaceType.EMPTY.value))
@@ -229,7 +229,157 @@ def traykloster(field):
     return score
 
 
-HOUSE_SCORING_CARDS = [
+def pfad_des_waldes(field):
+    connected_areas = ski.measure.label(
+        field == SpaceType.FOREST.value, background=False, connectivity=1
+    )
+
+    bonus_mountains_indices = set()
+    for area_label in np.unique(connected_areas):
+        if area_label == 0:
+            continue
+
+        surrounding_mask = ski.segmentation.find_boundaries(
+            connected_areas == area_label, mode="outer", connectivity=1
+        )
+
+        connected_mountains_indices = np.argwhere(
+            np.logical_and(surrounding_mask, field == SpaceType.MOUNTAIN.value)
+        )
+        if len(connected_mountains_indices) > 1:
+            bonus_mountains_indices.update(
+                set([tuple(idx) for idx in connected_mountains_indices])
+            )
+
+    return len(bonus_mountains_indices) * 3
+
+
+def schildwald(field):
+    edge = np.concatenate((field[0, :], field[-1, :], field[1:-1, 0], field[1:-1, -1]))
+
+    return np.count_nonzero(edge == SpaceType.FOREST.value)
+
+
+def gruenflaeche(field):
+    n_lines = 0
+
+    for row in field:
+        if SpaceType.FOREST.value in row:
+            n_lines += 1
+
+    for col in field.T:
+        if SpaceType.FOREST.value in col:
+            n_lines += 1
+
+    return n_lines
+
+
+def duesterwald(field):
+    score = 0
+
+    for forest_idx in np.argwhere(field == SpaceType.FOREST.value):
+        f = np.full(field.shape, 0)
+        f[forest_idx[0], forest_idx[1]] = 1
+        surrounding_mask = ski.segmentation.find_boundaries(
+            f, mode="outer", connectivity=1
+        )
+
+        if SpaceType.EMPTY.value not in field[np.argwhere(surrounding_mask)]:
+            score += 1
+
+    return score
+
+
+def goldener_kornspeicher(field):
+    # TODO this is the only scoring card in cartographers that uses the tempel field
+    # we need to implement the handling of the tempel field first
+    raise NotImplementedError()
+
+
+def tal_der_magier(field):
+    score = 0
+
+    for water_idx in np.argwhere(field == SpaceType.SEA.value):
+        f = np.full(field.shape, 0)
+        f[water_idx[0], water_idx[1]] = 1
+        surrounding_mask = ski.segmentation.find_boundaries(
+            f, mode="outer", connectivity=1
+        )
+
+        if SpaceType.MOUNTAIN.value in field[np.argwhere(surrounding_mask)]:
+            score += 2
+
+    for field_idx in np.argwhere(field == SpaceType.FIELD.value):
+        f = np.full(field.shape, 0)
+        f[field_idx[0], field_idx[1]] = 1
+        surrounding_mask = ski.segmentation.find_boundaries(
+            f, mode="outer", connectivity=1
+        )
+
+        if SpaceType.MOUNTAIN.value in field[np.argwhere(surrounding_mask)]:
+            score += 1
+
+    return score
+
+
+def bewaesserungskanal(field):
+    score = 0
+
+    for water_idx in np.argwhere(field == SpaceType.SEA.value):
+        f = np.full(field.shape, 0)
+        f[water_idx[0], water_idx[1]] = 1
+        surrounding_mask = ski.segmentation.find_boundaries(
+            f, mode="outer", connectivity=1
+        )
+
+        if SpaceType.FIELD.value in field[np.argwhere(surrounding_mask)]:
+            score += 1
+
+    for field_idx in np.argwhere(field == SpaceType.FIELD.value):
+        f = np.full(field.shape, 0)
+        f[field_idx[0], field_idx[1]] = 1
+        surrounding_mask = ski.segmentation.find_boundaries(
+            f, mode="outer", connectivity=1
+        )
+
+        if SpaceType.SEA.value in field[np.argwhere(surrounding_mask)]:
+            score += 1
+
+    return score
+
+
+def ausgedehnte_straende(field):
+    score = 0
+    for space_type_1, space_type_2 in [
+        (SpaceType.FIELD.value, SpaceType.SEA.value),
+        (SpaceType.SEA.value, SpaceType.FIELD.value),
+    ]:
+        areas = ski.measure.label(
+            field == space_type_1, background=False, connectivity=1
+        )
+        for area_label in np.unique(areas):
+            if area_label == 0:
+                continue
+
+            area = areas == area_label
+            area_indices = np.argwhere(area)
+            surrounding_mask = ski.segmentation.find_boundaries(
+                area, mode="outer", connectivity=1
+            )
+            surrounding_indices = np.argwhere(surrounding_mask)
+
+            if space_type_2 not in field[surrounding_indices] and np.all(
+                area_indices[:, 0] > 0
+                and area_indices[:, 0] < field.shape[0] - 1
+                and area_indices[:, 1] > 0
+                and area_indices[:, 1] < field.shape[1] - 1
+            ):
+                score += 3
+
+    return score
+
+
+SCORING_CARDS = [
     ScoringCard(
         "Bastionen in der Wildnis",
         134,
@@ -293,5 +443,69 @@ HOUSE_SCORING_CARDS = [
         traykloster,
         14,
         "7 Ruhmpunkte für jedes Dorf-Gebiet mit mindestens 1 ausgefüllten Rechteck der größe 1x4 (oder 4x1).",
+    ),
+    ScoringCard(
+        "Pfad des Waldes",
+        129,
+        TaskType.FOREST,
+        pfad_des_waldes,
+        18,
+        "3 Ruhmpunkte für jedes Gebirgs-Feld, das über mindestens 1 Wald-Gebiet mit mindestens 1 anderen Gebirgs-Feld verbunden ist.",
+    ),
+    ScoringCard(
+        "Schildwald",
+        126,
+        TaskType.FOREST,
+        schildwald,
+        25,
+        "1 Ruhmpunkt für jedes Wald-Feld das an den Rand grenzt.",
+    ),
+    ScoringCard(
+        "Grünfläche",
+        127,
+        TaskType.FOREST,
+        gruenflaeche,
+        22,
+        "1 Ruhmpunkt für jede Zeile und Spalte mit mindestens 1 Wald-Feld.",
+    ),
+    ScoringCard(
+        "Düsterwald",
+        128,
+        TaskType.FOREST,
+        duesterwald,
+        17,
+        "1 Ruhmpunkt für jedes Wald-Feld das an 4 ausgefüllte Felder (und/oder den Rand) angrenzt.",
+    ),
+    ScoringCard(
+        "Goldener Kornspeicher",
+        132,
+        TaskType.SEA_FIELD,
+        goldener_kornspeicher,
+        20,
+        "1 Ruhmpunkt für jedes Wasser-Feld, das an minestens 1 Ruinen-Feld grenzt. 3 Ruhmpunkte für jedes Acker-Feld, auf einem Ruinen-Feld.",
+    ),
+    ScoringCard(
+        "Tal der Magier",
+        131,
+        TaskType.SEA_FIELD,
+        tal_der_magier,
+        22,
+        "2 Ruhmpunkte für jedes Wasser-Feld, das an mindestens 1 Gebirgs-Feld grenzt. 1 Ruhmpunkt für jedes Acker-Feld, das an mindestens 1 Gebirgs-Feld grenzt.",
+    ),
+    ScoringCard(
+        "Bewässerungskanal",
+        130,
+        TaskType.SEA_FIELD,
+        bewaesserungskanal,
+        24,
+        "1 Ruhmpunkt für jedes Wasser-Feld, das an mindestens 1 Acker-Feld grenzt. 1 Ruhmpunkt für jedes Acker-Feld, das an mindestens 1 Wasser-Feld grenzt.",
+    ),
+    ScoringCard(
+        "Ausgedehnte Strände",
+        133,
+        TaskType.SEA_FIELD,
+        ausgedehnte_straende,
+        27,
+        "3 Ruhmpunkte für für jedes Acker-Gebiet, das weder an den Rand noch an 1 oder mehrere Wasser-Felder grenzt. 3 Ruhmpunkte für jedes Wasser-Gebiet, das weder an den Rand noch an 1 oder mehrere Acker-Felder grenzt.",
     ),
 ]

a8193e6b3e8ba9fefb76f579ed779a06ba2c158a
Moritz Hesche <mo.hesche@gmail.com>
2023-04-07 22:17:14 +0200

    dummy render

---
+++ carthographRL/game/view/pg.py
@@ -0,0 +1,31 @@
+import pygame as pg
+
+
+pg.init()
+screen = pg.display.det_mode((800,400))
+clock = pg.
+
+while True:
+
+
+# class GameController:
+#     def __init__(self, framerate):
+#         self.framerate = framerate
+
+#         pg.init()
+#         self.display = pg.display.set_mode((800, 400))
+#         self.clock = pg.time.Clock()
+
+#     def run(self):
+#         while True:
+#             pg.display.update()
+#             self.clock.tick(self.framerate)
+
+
+# def main():
+#     controller = GameController(5)
+#     controller.run()
+
+
+# if __name__ == "__main__":
+#     main()

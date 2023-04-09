2d8d0c0d89c80b87ecbe59210b5a9686c1472205
Moritz Hesche <mo.hesche@gmail.com>
2023-04-10 00:17:17 +0200

    switched to frozenset in map

--- carthographRL/game/model/map.py
+++ carthographRL/game/model/map.py
@@ -1,6 +1,6 @@
 """ Representation of the game map. """
 
-from typing import List, Tuple, Iterator
+from typing import List, Tuple, Iterator, FrozenSet
 
 import numpy as np
 import skimage as ski
@@ -14,13 +14,11 @@ class Map:
     def generate_A(cls):
         return cls(
             size=11,
-            ruin_coords=np.array(
-                [(1, 5), (2, 1), (2, 9), (8, 1), (8, 9), (9, 5)], dtype=int
+            ruin_coords=frozenset(
+                [(1, 5), (2, 1), (2, 9), (8, 1), (8, 9), (9, 5)],
             ),
-            mountain_coords=np.array(
-                [(1, 3), (2, 8), (5, 5), (8, 2), (9, 7)], dtype=int
-            ),
-            waste_coords=np.array([], dtype=int),
+            mountain_coords=frozenset([(1, 3), (2, 8), (5, 5), (8, 2), (9, 7)]),
+            waste_coords=frozenset([]),
         )
 
     @classmethod
@@ -48,165 +46,207 @@ class Map:
     def __init__(
         self,
         size: int,
-        ruin_coords: NDArray[Shape["Nr, 2"], Int],
-        mountain_coords: NDArray[Shape["Nm, 2"], Int],
-        waste_coords: NDArray[Shape["Nw, 2"], Int],
+        ruin_coords: FrozenSet[Tuple[int, int]],
+        mountain_coords: FrozenSet[Tuple[int, int]],
+        waste_coords: FrozenSet[Tuple[int, int]],
     ):
         """Initialize a new map.
 
         Args:
             size (int): The size of the square map.
-            ruin_coords (NDArray[Shape["Nr, 2"], Int]): The coordinates of the ruins.
-            mountain_coords (NDArray[Shape["Nm, 2"], Int]): The coordinates of the mountains.
-            waste_coords (NDArray[Shape["Nw, 2"], Int]): The coordinates of the wastelands.
+            ruin_coords (FrozenSet[Tuple[int, int]]): The coordinates of the ruins.
+            mountain_coords (FrozenSet[Tuple[int, int]]): The coordinates of the mountains.
+            waste_coords (FrozenSet[Tuple[int, int]]): The coordinates of the wastelands.
         """
         self.size = size
         self.terrain_map = np.full((size, size), Terrains.EMPTY.value)
 
         for mountain_coord in mountain_coords:
-            self.terrain_map[mountain_coord] = Terrains.MOUNTAIN.value
+            self.terrain_map[
+                mountain_coord[0], mountain_coord[1]
+            ] = Terrains.MOUNTAIN.value
         for waste_coord in waste_coords:
-            self.terrain_map[waste_coord] = Terrains.WASTE.value
+            self.terrain_map[waste_coord[0], waste_coord[1]] = Terrains.WASTE.value
 
         # ruins are not a terrain, but a special case
         self.ruin_coords = ruin_coords
 
-    def _transform_to_map_coords(
+    def transform_to_map_coords(
         self,
-        shape_coords: NDArray[Shape["N, 2"], Int],
+        shape_coords: FrozenSet[Tuple[int, int]],
         rotation: int,
-        position: NDArray[Shape["2"], Int],
+        position: FrozenSet[Tuple[int, int]],
         mirror: bool,
-    ) -> NDArray[Shape["N, 2"], Int]:
+    ) -> FrozenSet[Tuple[int, int]]:
         """Transforms the coordinates of a piece to the map coordinates depending on the rotation and the position.
         Does not check if the transformed coordinates are valid.
 
         Args:
-            shape_coords (NDArray[Shape["N, 2"], Int]): The coordinates of the piece, i.e. its shape. (NOT the map coordinates)
+            shape_coords ( FrozenSet[Tuple[int, int]]): The coordinates of the piece, i.e. its shape. (NOT the map coordinates)
             rotation (int): The rotation of the piece in mulitples of 90 degrees. (1=90deg, 2=180deg, ...)
-            position (NDArray[Shape["2"], Int]): The position of the piece.
+            position (Tuple[int, int]): The position of the piece.
             mirror (bool): Whether the piece is mirrored.
 
         Returns:
-            NDArray[Shape["N, 2"], Int]: The transformed coordinates.
+             FrozenSet[Tuple[int, int]]: The transformed coordinates.
         """
         if mirror:
-            shape_coords = np.array([(-x, y) for x, y in shape_coords])
+            shape_coords = {(-x, y) for x, y in shape_coords}
 
         if rotation == 0:
-            shape_coords = np.array(shape_coords)
+            shape_coords = shape_coords
         elif rotation == 1:
-            shape_coords = np.array([(y, -x) for x, y in shape_coords])
+            shape_coords = {(y, -x) for x, y in shape_coords}
         elif rotation == 2:
-            shape_coords = np.array([(-x, -y) for x, y in shape_coords])
+            shape_coords = {(-x, -y) for x, y in shape_coords}
         elif rotation == 3:
-            shape_coords = np.array([(-y, x) for x, y in shape_coords])
+            shape_coords = {(-y, x) for x, y in shape_coords}
         else:
             raise ValueError(f"Invalid rotation: {rotation}")
 
-        shape_coords[:, 0] -= np.min(shape_coords[:, 0])
-        shape_coords[:, 1] -= np.min(shape_coords[:, 1])
+        min_x = min(x for x, _ in shape_coords)
+        min_y = min(y for _, y in shape_coords)
+        shape_coords = {(x - min_x, y - min_y) for x, y in shape_coords}
 
-        shape_coords = shape_coords + position
+        shape_coords = {(x + position[0], y + position[1]) for x, y in shape_coords}
 
-        return shape_coords
+        return frozenset(shape_coords)
 
-    def _transform_to_cluster(
+    def transform_to_cluster(
         self,
-        shape_coords: NDArray[Shape["N, 2"], Int],
+        shape_coords: FrozenSet[Tuple[int, int]],
         rotation: int,
-        position: Tuple[Int, Int],
+        position: Tuple[int, int],
         mirror: bool,
     ) -> "Cluster":
         """Same as transform_to_map_coords, but returns a Cluster object.
 
         Args:
-            shape_coords (NDArray[Shape["N, 2"], Int]): The coordinates of the piece, i.e. its shape. (NOT the map coordinates)
+            shape_coords (FrozenSet[Tuple[int, int]]): The coordinates of the piece, i.e. its shape. (NOT the map coordinates)
             rotation (int): The rotation of the piece in mulitples of 90 degrees. (1=90deg, 2=180deg, ...)
-            position (Tuple[Int, Int]): The position of the piece.
+            position (Tuple[int, int]): The position of the piece.
             mirror (bool): Whether the piece is mirrored.
 
         Returns:
             Cluster: The transformed coordinates as a cluster object.
         """
-        cluster_coords = self._transform_to_map_coords(
+        cluster_coords = self.transform_to_map_coords(
             shape_coords, rotation, position, mirror
         )
         return Cluster(cluster_coords, self)
 
     def place(
         self,
-        shape_coords: NDArray[Shape["N, 2"], Int],
+        shape_coords: FrozenSet[Tuple[int, int]],
         terrain: Terrains,
         rotation: int,
-        position: NDArray[Shape["2"], Int],
+        position: Tuple[int, int],
         mirror: bool,
         on_ruin: bool,
     ):
         """Places a piece on the map. Validates the move.
 
         Args:
-            coords (NDArray[Shape["N, 2"], Int]): The original coordinates of the piece,
+            coords (FrozenSet[Tuple[int, int]]): The original coordinates of the piece,
                 i.e. its shape. (NOT the map coordinates)
             terrain (Terrains): The terrain of the piece.
             rotation (int): The rotation of the piece in mulitples of 90 degrees. (1=90deg, 2=180deg, ...)
-            position (NDArray[Shape["2"], Int]): The position of the piece.
+            position (Tuple[int, int]): The position of the piece.
             mirror (bool): Whether the piece is mirrored.
             on_ruin (bool): Whether the piece must be placed on a ruin.
 
         Raises:
             InvalidMoveError: If the piece cannot be placed on the map.
         """
-        cluster = self._transform_to_cluster(shape_coords, rotation, position, mirror)
+        cluster = self.transform_to_cluster(shape_coords, rotation, position, mirror)
         cluster.is_valid(on_ruin)
-        self.terrain_map[shape_coords] = terrain.value
+        x_idx, y_idx = zip(*cluster.coords)
+        self.terrain_map[x_idx, y_idx] = terrain.value
 
-    def is_setable(self, coords, rotation, position, mirror, on_ruin):
-        """Checks if a piece can be placed on the map.
+    def is_setable(
+        self,
+        coords: FrozenSet[Tuple[int, int]],
+        rotation: int,
+        position: Tuple[int, int],
+        mirror: bool,
+        on_ruin: bool,
+    ):
+        """Checks if an intended move is valid.
 
         Args:
-            coords (NDArray[Shape["N, 2"], Int]): The original coordinates of the piece,
+            coords (FrozenSet[Tuple[int, int]]): The original coordinates of the piece,
                 i.e. its shape. (NOT the map coordinates)
             rotation (int): The rotation of the piece in mulitples of 90 degrees. (1=90deg, 2=180deg, ...)
-            position (NDArray[Shape["2"], Int]): The position of the piece.
+            position (): The position of the piece.
             mirror (bool): Whether the piece is mirrored.
             on_ruin (bool): Whether the piece must be placed on a ruin.
 
         Returns:
-            bool: Whether the piece can be placed on the map.
+            bool: Whether the move is valid.
         """
-        cluster = self._transform_to_cluster(coords, rotation, position, mirror)
+        cluster = self.transform_to_cluster(coords, rotation, position, mirror)
         return cluster.is_valid(on_ruin, raise_error=False)
 
     def setable_options(
-        self, shape_coords: NDArray[Shape["N, 2"], Int], on_ruin: bool
-    ) -> Iterator[
-        Tuple[NDArray[Shape["N, 2"], Int], int, NDArray[Shape["2"], Int], bool]
-    ]:
-        """Returns all possible placements of a piece on the map.
+        self, shape_coords: FrozenSet[Tuple[int, int]], on_ruin: bool
+    ) -> Iterator[int, Tuple[int, int], bool]:
+        """Returns all valid options which result in a valid move.
+        Note that some options may lead to the same move.
 
         Args:
-            coords (NDArray[Shape["N, 2"], Int]): The original coordinates of the piece,
+            coords (FrozenSet[Tuple[int, int]]): The original coordinates of the piece,
                 i.e. its shape. (NOT the map coordinates)
             on_ruin (bool): Whether the piece must be placed on a ruin.
 
         Yields:
-            Tuple[NDArray[Shape["N, 2"], Int], int, NDArray[Shape["2"], Int], bool]: The possible placements. (shape_coords, rotation, position, mirror)
+            Tuple[int, Tuple[int, int], bool]: The possible placements. (rotation, position, mirror)
         """
         for x in range(self.terrain_map.shape[0]):
             for y in range(self.terrain_map.shape[1]):
                 for r in range(4):
                     for mirror in [False, True]:
-                        if self._transform_to_cluster(
-                            shape_coords, r, (x, y), mirror
-                        ).is_valid(on_ruin):
-                            yield (shape_coords, r, (x, y), mirror)
+                        if self.is_setable(shape_coords, r, (x, y), mirror, on_ruin):
+                            yield (r, (x, y), mirror)
+
+    # def setable_clusters(
+    #     self, shape_coords: FrozenSet[Tuple[int, int]], on_ruin: bool
+    # ) -> FrozenSet["Cluster"]:
+    #     """Returns all possible results of valid moves.
+
+    #     Args:
+    #         coords (FrozenSet[Tuple[int, int]]): The original coordinates of the piece,
+    #             i.e. its shape. (NOT the map coordinates)
+    #         on_ruin (bool): Whether the piece must be placed on a ruin.
+
+    #     Returns:
+    #         FrozenSet[Cluster]: The possible placements.
+    #     """
+    #     setable_options = self.setable_options(shape_coords, on_ruin=on_ruin)
+
+    #     setable_clusters = frozenset(
+    #         {
+    #             self.transform_to_cluster(shape_coords, rotation, position, mirror)
+    #             for rotation, position, mirror in setable_options
+    #         }
+    #     )
+
+    #     return setable_clusters
 
     def is_setable_anywhere(
         self,
-        coords: NDArray[Shape["N, 2"], Int],
+        coords: FrozenSet[Tuple[int, int]],
         on_ruin: bool,
     ) -> bool:
+        """Checks if a piece can be placed anywhere on the map.
+
+        Args:
+            coords (FrozenSet[Tuple[int, int]]): The original coordinates of the piece,
+                i.e. its shape. (NOT the map coordinates)
+            on_ruin (bool): Whether the piece must be placed on a ruin.
+
+        Returns:
+            bool: Whether the piece can be placed anywhere on the map.
+        """
         try:
             next(self.setable_options(coords, on_ruin))
             return True
@@ -216,7 +256,7 @@ class Map:
     def eval_monsters(self) -> int:
         """Gets the number of empty fields which are adjacent to a monster."""
         cluster = Cluster(np.argwhere(self.terrain_map == Terrains.MONSTER.value), self)
-        return sum(t == Terrains.EMPTY for t in cluster.surrounding_terrains())
+        return sum(t == Terrains.EMPTY.value for t in cluster.surrounding_terrains())
 
     def get_surrounded_mountains(self) -> int:
         """Returns the number of mountains that are fully surrounded.
@@ -227,42 +267,44 @@ class Map:
         coords = np.argwhere(self.terrain_map == Terrains.MOUNTAIN.value)
         return sum([Cluster(c, self).is_surrounded() for c in coords])
 
-    def clusters(self, terrain_type: Terrains) -> List["Cluster"]:
+    def clusters(self, terrain_type: Terrains) -> FrozenSet["Cluster"]:
         """Returns all clusters of a terrain type.
 
         Args:
             terrain_type (Terrains): The terrain type.
 
         Returns:
-            List[Cluster]: The clusters.
+            FrozenSet[Cluster]: The clusters.
         """
         cluster_map = ski.measure.label(
             self.terrain_map == terrain_type.value, background=False, connectivity=1
         )
 
-        return [
-            Cluster(np.argwhere(cluster_map == cluster_label), self)
-            for cluster_label in np.unique(cluster_map)
-            if cluster_label != 0
-        ]
+        return frozenset(
+            {
+                Cluster(np.argwhere(cluster_map == cluster_label), self)
+                for cluster_label in np.unique(cluster_map)
+                if cluster_label != 0
+            }
+        )
 
-    def __getitem__(self, key):
-        return self.terrain_map[key]
+    # def __getitem__(self, key):
+    #     return self.terrain_map[key]
 
 
 class Cluster:
-    def __init__(self, coords: NDArray[Shape["N, 2"], Int], map_sheet: Map):
+    def __init__(self, coords: FrozenSet[Tuple[int, int]], map_sheet: Map):
         """Representation of a cluster of coordinates in context of the map.
         Contains different methods to get information about the cluster.
         Does not modify the map at any operation.
         Intended to prevent overloading the map class with methods.
 
         Args:
-            coords (NDArray[Shape["N, 2"], Int]): The coordinates.
+            coords (FrozenSet[Tuple[int, int]]): The coordinates.
             map_sheet (Map): The map sheet.
         """
         self.coords = coords
-        self.map_sheet = map_sheet
+        self._map_sheet = map_sheet
 
     def surrounding_mask(self) -> NDArray[Shape["S, S"], Bool]:
         """Returns a mask of the surrounding area of the cluster.
@@ -270,15 +312,16 @@ class Cluster:
         Returns:
             NDArray[Shape["S, S"], Bool]: The mask.
         """
-        f = np.full(self.map_sheet.terrain_map.shape, False)
-        f[self.coords] = True
+        f = np.full(self._map_sheet.terrain_map.shape, False)
+        x_idx, y_idx = zip(*self.coords)
+        f[x_idx, y_idx] = True
         return ski.segmentation.find_boundaries(f, mode="outer", connectivity=1)
 
-    def surrounding_coords(self) -> NDArray[Shape["N, 2"], Int]:
+    def surrounding_coords(self) -> FrozenSet[Tuple[int, int]]:
         """Returns the coordinates of the surrounding area of the cluster.
 
         Returns:
-            NDArray[Shape["N, 2"], Int]: The coordinates.
+             FrozenSet[Tuple[int, int]]: The coordinates.
         """
         return np.argwhere(self.surrounding_mask())
 
@@ -289,18 +332,19 @@ class Cluster:
         Returns:
             NDArray[Shape["S, S"], Int]: The surrounding map.
         """
-        f = self.map_sheet.terrain_map.copy()
+        f = self._map_sheet.terrain_map.copy()
         f[not self.surrounding_mask()] = -1
 
         return f
 
-    def surrounding_terrains(self) -> NDArray[Shape["N"], Int]:
+    def surrounding_terrains(self) -> List[int]:
         """Returns the terrains of the surrounding area of the cluster.
 
         Returns:
-            NDArray[Shape["N"], Int]: The terrains.
+            List[int]: The terrains.
         """
-        return self.map_sheet.terrain_map[self.surrounding_coords()]
+        x_idx, y_idx = zip(*self.surrounding_coords())
+        return self._map_sheet.terrain_map[x_idx, y_idx].tolist()
 
     def on_edge(self) -> bool:
         """Returns whether the cluster has at least one element which is on the edge of the map.
@@ -309,8 +353,9 @@ class Cluster:
             bool: Whether the cluster is on the edge.
         """
         return any(
-            c in (0, self.map_sheet.terrain_map.shape[0] - 1)
-            for c in self.coords.flatten()
+            c in (0, self._map_sheet.terrain_map.shape[0] - 1)
+            for coord_tuple in self.coords
+            for c in coord_tuple
         )
 
     def on_map(self) -> bool:
@@ -333,7 +378,8 @@ class Cluster:
         Returns:
             bool: Whether the cluster is on a ruin.
         """
-        return any(c in self.map.ruin_coords for c in self.coords)
+        # return any(c in self.map.ruin_coords for c in self.coords)
+        return len(self.coords.intersection(self._map_sheet.ruin_coords)) > 0
 
     def is_surrounded(self) -> bool:
         """Returns whether the cluster is completely surrounded by other terrains or on the edge of the map.
@@ -350,7 +396,7 @@ class Cluster:
             bool: Whether the cluster is occupied.
         """
         return any(
-            self.map_sheet.terrain_map[coord] != Terrains.EMPTY.value
+            self._map_sheet.terrain_map[coord[0], coord[1]] != Terrains.EMPTY.value
             for coord in self.coords
         )
 
@@ -386,3 +432,6 @@ class Cluster:
 
     def __len__(self):
         return len(self.coords)
+
+    def __hash__(self) -> int:
+        return self.coords.__hash__()
